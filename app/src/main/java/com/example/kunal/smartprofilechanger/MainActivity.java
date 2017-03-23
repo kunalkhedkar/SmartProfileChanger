d0e173a58015860dad21b5faa3d4d0aa3a57b366
kunalkhedkar <kunalvkhedkar@gmail.com>
2017-03-23 23:45:03 +0530

    2nd commit

--- app/src/main/java/com/example/kunal/smartprofilechanger/MainActivity.java
+++ app/src/main/java/com/example/kunal/smartprofilechanger/MainActivity.java
@@ -1,22 +1,36 @@
 package com.example.kunal.smartprofilechanger;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.media.AudioManager;
 import android.os.Bundle;
+
 import android.support.annotation.NonNull;
 import android.support.multidex.MultiDex;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.awareness.Awareness;
-import com.google.android.gms.awareness.snapshot.HeadphoneStateResult;
-import com.google.android.gms.awareness.state.HeadphoneState;
+
+import com.google.android.gms.awareness.snapshot.LocationResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
+
 
 public class MainActivity extends Activity {
 
     private final static String TAG = "mainActivity";
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
+    private static final String ACCESS_FINE_LOCATION = "android.permission.ACCESS_FINE_LOCATION";
     private GoogleApiClient mGoogleApiClient;
     private TextView status_tv;
     boolean statusFlag = false;
@@ -45,41 +59,68 @@ public class MainActivity extends Activity {
     }
 
 
+    public void onclick_changeProfileButton(View view) {
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+    }
 
 
     public void onClickCheckButton(View view) {
 
-        if (CheckHeadphoneStatus()) {
-            status_tv.setText("headphone is plugged in");
-        } else {
+        getCurrentLocation();
+    }
 
-            status_tv.setText("headphone is NOT plugged in");
+    private void getCurrentLocation() {
+
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setCancelable(false);
+        dialog.show();
+
+        if (checkLocationPermission()) {
+            // Toast.makeText(this, "write code", Toast.LENGTH_SHORT).show();
+            status_tv.setText("");
+
+            Awareness.SnapshotApi.getLocation(mGoogleApiClient).
+                    setResultCallback(new ResultCallback<LocationResult>() {
+                        @Override
+                        public void onResult(@NonNull LocationResult locationResult) {
+                            if (!locationResult.getStatus().isSuccess()) {
+                                status_tv.setText("Could not get location");
+                                status_tv.setTextColor(Color.RED);
+                                return;
+                            } else {
+                                Location location = locationResult.getLocation();
+                                status_tv.setText("loc is : " + location.toString());
+                                Log.d(TAG, "onResult: " + location.toString());
+                                status_tv.setTextColor(Color.GREEN);
+                            }
+                        }
+                    });
+
         }
+        dialog.dismiss();
     }
 
+    private boolean checkLocationPermission() {
 
-    boolean CheckHeadphoneStatus() {
-        final boolean flag=false;
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-        Awareness.SnapshotApi.getHeadphoneState(mGoogleApiClient).setResultCallback(new ResultCallback<HeadphoneStateResult>() {
-            @Override
-            public void onResult(@NonNull HeadphoneStateResult headphoneStateResult) {
+            Toast.makeText(this, "No Permission granted", Toast.LENGTH_SHORT).show();
 
-                if (headphoneStateResult.getStatus().isSuccess()) {
-                    HeadphoneState headphonestate = headphoneStateResult.getHeadphoneState();
-                    if (headphonestate.getState() == HeadphoneState.PLUGGED_IN) {
-                        statusFlag = true;
-                    } else {
-                        statusFlag = false;
-                    }
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+        } else {
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            // Awareness API code goes here
+            return true;
+        }
 
 
-                }
-            }
-        });
-        return statusFlag;
+        return false;
     }
-
 
 
 }
